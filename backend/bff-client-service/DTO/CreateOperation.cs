d7ab9c141bc2e0e5d7f6d92ba94a1f9fb32800a3
lambrussco <lerkal.v.m@gmail.com>
2023-04-16 14:26:07 +0700

    some fixes

--- backend/bff-client-service/DTO/CreateOperation.cs
+++ backend/bff-client-service/DTO/CreateOperation.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace bff_client_service.DTO
 {
 	public class CreateOperation
 	{
-		public CreateOperation()
+        [Required]
+        public Guid UserID { get; set; }
+        [Required]
+        public int AccountNumber { get; set; }
+        [Required]
+        public DateTime DateTime { get; set; }
+        [Required]
+        public decimal TransactionAmount { get; set; }
+        public CreateOperation()
 		{
 		}
 	}
